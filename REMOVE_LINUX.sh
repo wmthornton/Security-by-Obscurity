6e111fc733cdcda8f33b93a8676fd32faaef63a5
Wayne Thornton <wmthornton.dev@outlook.com>
2021-08-20 07:49:28 -0600

    Implemented EUID check for ins and rem scripts

--- REMOVE_LINUX.sh
+++ REMOVE_LINUX.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-sudo make remove
+# bool function to test if the user is root or not (POSIX only)
+is_user_root () { [ "$(id -u)" -eq 0 ]; }
+
+if is_user_root; then
+    echo 'You are the almighty root!'
+    sudo make remove
+    exit 0 # implicit, here it serves the purpose to be explicit for the reader
+else
+    echo 'You are just an ordinary user. This script must be run with sudo permissions.' >&2
+    exit 1
+fi
